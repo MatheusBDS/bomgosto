c9eea394f8ca6b2f9d284de8e24ffbf04a6269a4
Matheus Barbosa <matheusb565@gmail.com>
2020-02-19 17:38:25 -0300

    Implementação Categoria

--- src/main/java/com/bomgosto/resources/CategoriaResource.java
+++ src/main/java/com/bomgosto/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.bomgosto.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bomgosto.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST OK";
+	public List<Categoria> listar() {
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(Categoria.builder()
+				.id(1)
+				.nome("Informática")
+				.build());
+		lista.add(Categoria.builder()
+				.id(2)
+				.nome("Escritório")
+				.build());
+		
+		return lista;
 	}
-	
+
 }
