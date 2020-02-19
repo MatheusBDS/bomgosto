ae189539a16142f7dea1c011fe015c09a15eff4d
Matheus Barbosa <matheusb565@gmail.com>
2020-02-19 16:15:35 -0300

    Projeto criado

---
+++ src/test/java/com/bomgosto/BomgostoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bomgosto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomgostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
