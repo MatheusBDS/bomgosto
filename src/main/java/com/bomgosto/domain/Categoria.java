0cf912f174abfed1a9c68465f11bfa2f400af159
Matheus Barbosa <matheusb565@gmail.com>
2020-02-19 18:48:32 -0300

    Criação automática da base para teste com H2

--- src/main/java/com/bomgosto/domain/Categoria.java
+++ src/main/java/com/bomgosto/domain/Categoria.java
@@ -2,6 +2,11 @@ package com.bomgosto.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -15,9 +20,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
