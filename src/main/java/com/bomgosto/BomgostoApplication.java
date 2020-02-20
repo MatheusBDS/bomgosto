1ba96cb4f59069ce3b9e01491d05fc145a44bc64
Matheus Barbosa <matheusb565@gmail.com>
2020-02-20 17:22:50 -0300

    Criando operação de instanciação

--- src/main/java/com/bomgosto/BomgostoApplication.java
+++ src/main/java/com/bomgosto/BomgostoApplication.java
@@ -1,13 +1,34 @@
 package com.bomgosto;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BomgostoApplication {
+import com.bomgosto.domain.Categoria;
+import com.bomgosto.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class BomgostoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BomgostoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		categoriaRepository.saveAll(Arrays.asList(
+				Categoria.builder()
+				.id(null)
+				.nome("Informática").build(),
+				Categoria.builder()
+				.id(null)
+				.nome("Escritório").build()));
+	}
+	
 }
