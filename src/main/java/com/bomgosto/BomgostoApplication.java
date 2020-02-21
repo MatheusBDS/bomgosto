e071ab647f8a25ea151478d06baaf5160876874b
Matheus Barbosa <matheusb565@gmail.com>
2020-02-21 13:49:38 -0300

    Produto e associação de muito para muitos

--- src/main/java/com/bomgosto/BomgostoApplication.java
+++ src/main/java/com/bomgosto/BomgostoApplication.java
@@ -1,5 +1,7 @@
 package com.bomgosto;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.bomgosto.domain.Categoria;
+import com.bomgosto.domain.Produto;
 import com.bomgosto.repositories.CategoriaRepository;
+import com.bomgosto.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class BomgostoApplication implements CommandLineRunner{
@@ -16,19 +20,61 @@ public class BomgostoApplication implements CommandLineRunner{
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BomgostoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		categoriaRepository.saveAll(Arrays.asList(
-				Categoria.builder()
+		
+		// PRODUTOS
+		Produto p1 = Produto.builder()
 				.id(null)
-				.nome("Informática").build(),
-				Categoria.builder()
+				.nome("Pizza de Calabresa")
+				.tamanho("Grande")
+				.unidadeMedida("Gramas")
+				.precoUnitario(new BigDecimal(30))
+				.categorias(new ArrayList<>())
+				.build();
+		Produto p2 = Produto.builder()
 				.id(null)
-				.nome("Escritório").build()));
+				.nome("Pizza de Frango")
+				.tamanho("Grande")
+				.unidadeMedida("Gramas")
+				.precoUnitario(new BigDecimal(30))
+				.categorias(new ArrayList<>())
+				.build();
+		Produto p3 = Produto.builder()
+				.id(null)
+				.nome("Suco de Laranja")
+				.tamanho("500")
+				.unidadeMedida("Milimetros")
+				.precoUnitario(new BigDecimal(3))
+				.categorias(new ArrayList<>())
+				.build();
+		
+		// CATEGORIAS
+		Categoria cat1 = Categoria.builder()
+				.id(null)
+				.nome("Pizza")
+				.produtos(new ArrayList<>())
+				.build();
+		Categoria cat2 = Categoria.builder()
+				.id(null)
+				.nome("Bebida")
+				.produtos(new ArrayList<>())
+				.build();
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat2));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 	
 }
--- src/main/java/com/bomgosto/domain/Produto.java
+++ src/main/java/com/bomgosto/domain/Produto.java
@@ -1,12 +1,15 @@
 package com.bomgosto.domain;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.AllArgsConstructor;
@@ -23,15 +26,23 @@ import lombok.Setter;
 @EqualsAndHashCode
 @Builder
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos;
+	private String tamanho;
+	private String unidadeMedida;
+	private BigDecimal precoUnitario;
+	private String imagem;
+	
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", 
+		joinColumns = @JoinColumn(name = "produto_id"), 
+		inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias;
+	
 
 }
