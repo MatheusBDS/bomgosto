92149daba5dc149a95f09debeeb0ff16f72481b0
Matheus Barbosa <matheusb565@gmail.com>
2020-05-25 23:01:16 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/bomgosto/dto/CategoriaDTO.java
+++ src/main/java/com/bomgosto/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.bomgosto.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.bomgosto.domain.Categoria;
 
 import lombok.Builder;
@@ -18,6 +22,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	@Builder
--- src/main/java/com/bomgosto/services/CategoriaService.java
+++ src/main/java/com/bomgosto/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.bomgosto.domain.Categoria;
+import com.bomgosto.dto.CategoriaDTO;
 import com.bomgosto.repositories.CategoriaRepository;
 import com.bomgosto.services.exceptions.DataIntegrityException;
 import com.bomgosto.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,12 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return Categoria.builder()
+				.id(objDTO.getId())
+				.nome(objDTO.getNome())
+				.build();
 	}
 }
