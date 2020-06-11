16e1cbbe8607c5c52df19237fc01bdcb286fb354
Matheus Barbosa <37903440+MatheusBDS@users.noreply.github.com>
2020-06-10 22:18:20 -0300

    Ajustes em PedidoService

--- src/main/java/com/bomgosto/services/PedidoService.java
+++ src/main/java/com/bomgosto/services/PedidoService.java
@@ -1,20 +1,6 @@
 package com.bomgosto.services;
 
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.Optional;
-
-import javax.transaction.Transactional;
-
 import com.bomgosto.domain.Cliente;
-import com.bomgosto.security.UserSS;
-import com.bomgosto.services.exceptions.AuthorizationException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-
 import com.bomgosto.domain.ItemPedido;
 import com.bomgosto.domain.PagamentoComBoleto;
 import com.bomgosto.domain.Pedido;
@@ -22,72 +8,84 @@ import com.bomgosto.domain.enums.EstadoPagamento;
 import com.bomgosto.repositories.ItemPedidoRepository;
 import com.bomgosto.repositories.PagamentoRepository;
 import com.bomgosto.repositories.PedidoRepository;
+import com.bomgosto.security.UserSS;
+import com.bomgosto.services.exceptions.AuthorizationException;
 import com.bomgosto.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class PedidoService {
 
-	@Autowired
-	private PedidoRepository repo;
+    @Autowired
+    private PedidoRepository repo;
 
-	@Autowired
-	private BoletoService boletoService;
-	
-	@Autowired
-	private PagamentoRepository pagamentoRepository;
-	
-	@Autowired
-	private ClienteService clienteService;
-	
-	@Autowired
-	private ProdutoService produtoService;
-	
-	@Autowired
-	private ItemPedidoRepository itemPedidoRepository;
-	
-	@Autowired
-	private EmailService emailService;
-	
-	public Pedido find(Integer id){
-		Optional<Pedido> obj = repo.findById(id);
-		
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id " + id + ", Tipo: " + Pedido.class.getName()));
-	}
-	
-	@Transactional
-	public Pedido insert(Pedido obj) {
-		obj.setId(null);
-		obj.setInstante(new Date());
-		obj.setObservacoes(obj.getObservacoes());
-		obj.setCliente(clienteService.find(obj.getCliente().getId()));
-		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
-		obj.getPagamento().setPedido(obj);
-		if (obj.getPagamento() instanceof PagamentoComBoleto) {
-			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
-			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
-		}
-		obj = repo.save(obj);
-		pagamentoRepository.save(obj.getPagamento());
-		for (ItemPedido ip : obj.getItens()) {
-			ip.setDesconto(BigDecimal.valueOf(0));
-			ip.setProduto(produtoService.find(ip.getProduto().getId()));
-			ip.setPreco(ip.getProduto().getPrecoUnitario());
-			ip.setPedido(obj);
-		}
-		itemPedidoRepository.saveAll(obj.getItens());
-		emailService.sendOrderConfirmationHtmlEmail(obj);	
-		return obj;
-	}
-	
-	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		UserSS user = UserService.authenticated();
-		if (!Optional.ofNullable(user).isPresent()) {
-			throw new AuthorizationException("Acesso negado");
-		}
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-		Cliente cliente =  clienteService.find(user.getId());
-		return repo.findByCliente(cliente, pageRequest);
-	}
+    @Autowired
+    private BoletoService boletoService;
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    public Pedido find(Integer id) {
+        Optional<Pedido> obj = repo.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id " + id + ", Tipo: " + Pedido.class.getName()));
+    }
+
+    @Transactional
+    public Pedido insert(Pedido obj) {
+        obj.setId(null);
+        obj.setInstante(new Date());
+        obj.setObservacoes(obj.getObservacoes());
+        obj.setCliente(clienteService.find(obj.getCliente().getId()));
+        obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+        obj.getPagamento().setPedido(obj);
+        if (obj.getPagamento() instanceof PagamentoComBoleto) {
+            PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+            boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+        }
+        obj = repo.save(obj);
+        pagamentoRepository.save(obj.getPagamento());
+        for (ItemPedido ip : obj.getItens()) {
+            ip.setDesconto(BigDecimal.valueOf(0));
+            ip.setProduto(produtoService.find(ip.getProduto().getId()));
+            ip.setPreco(ip.getProduto().getPrecoUnitario());
+            ip.setPedido(obj);
+        }
+        itemPedidoRepository.saveAll(obj.getItens());
+        emailService.sendOrderConfirmationHtmlEmail(obj);
+        return obj;
+    }
+
+    public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        UserSS user = UserService.authenticated();
+        if (!Optional.ofNullable(user).isPresent()) {
+            throw new AuthorizationException("Acesso negado");
+        }
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Cliente cliente = clienteService.find(user.getId());
+        return repo.findByCliente(cliente, pageRequest);
+    }
 
 }
